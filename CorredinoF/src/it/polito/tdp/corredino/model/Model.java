da7d2008af5f8e80698a420771a040a4ce8c5939
chiaralansia <Chiara Lanza@DESKTOP-3GGFU0F>
2019-06-06 10:31:29 +0200

    Aggiornamenti

--- CorredinoF/src/it/polito/tdp/corredino/model/Model.java
+++ CorredinoF/src/it/polito/tdp/corredino/model/Model.java
@@ -15,15 +15,15 @@ public class Model {
 	
 	List<Product> allP =new ArrayList<>();
 	List<List<Product>> allRes = new ArrayList<>();
-	List<Product> candidata = new ArrayList<>();
+	List<Product> candidata;
 	List<Product> best= new ArrayList<>();
 	float totBest=Float.MAX_VALUE;
 	List<String> cat= new ArrayList<>();
 	Map<String,Integer> controllore = new HashMap<>();
-	Map<String, List<Product>> categories = new HashMap<String,List<Product>>();
+	//Map<String, List<Product>> categories = new HashMap<String,List<Product>>();
 	float tot =0;
 	List<Product> rimanenti; //new ArrayList<>();
-	List<Product> scartati = new ArrayList<>();
+	//List<Product> scartati = new ArrayList<>();
 
 	
 	public Model() {
@@ -38,6 +38,7 @@ public class Model {
 //	}
 	
 	public List<Product> calcola(float budget, String season) {
+		candidata = new ArrayList<>();
 		if (season.equals("Autunno/Primavera"))
 				season = "Privavera";
 		allP=dao.getAllProduct(season);
@@ -55,25 +56,25 @@ public class Model {
 	
 
 private void prova(List<Product> candidata, int L, float budget) {
-		if(budget-tot<50) {
-			System.out.println(candidata.toString());
-		if(this.isCompleta(candidata) && tot<budget) {
+		
+		if(this.isCompleta(candidata) && tot<budget && budget-tot<50 && !candidata.isEmpty()) {
 			allRes.add(candidata);
 			System.out.println(candidata.toString());
-			if(tot<totBest && budget-tot<50) {
+			if(tot<totBest) {// && budget-tot<50) {
+				System.out.println(candidata.toString());
 				best= new ArrayList<>(candidata);
 				totBest=tot;
 			}
-	}}else if(!(L==dao.totProdotti())) {
+	} else if(!(L==dao.totProdotti())) {
 		rimanenti = new ArrayList<>(allP);
-		rimanenti.removeAll(candidata);
-		rimanenti.removeAll(scartati);
+		rimanenti.removeAll(candidata); 
+		//rimanenti.removeAll(scartati);
 		for(Product p: rimanenti) {
 			if(aggiuntaValida(p,candidata, budget)) {
 				candidata.add(p);
 				prova(candidata, L+1, budget);
 				candidata.remove(candidata.size()-1);
-			} else scartati.add(p);
+			}// else scartati.add(p);
 			
 			//come faccio a bloccare se ho raggiunto il budget ma non è completa?
 		}
@@ -84,13 +85,14 @@ private void prova(List<Product> candidata, int L, float budget) {
 private boolean aggiuntaValida(Product p, List<Product> candidata2, float budget) {
 	for (String c: cat) {
 		int i=0;
-		if(!candidata2.isEmpty())
+		if(candidata2.size()!=0)
 		for(Product pt: candidata2) {
 			if(c.equals(pt.getCategory())) {
 				i++;
 			}	
 		}
-		controllore.replace(c, i);
+		controllore.remove(c);
+		controllore.put(c, i);
 	}
 	
 	//la quantità di body, ghettine, calzini e tutine deve essere la stessa, massimo 5 minimo 2
@@ -123,7 +125,7 @@ private boolean aggiuntaValida(Product p, List<Product> candidata2, float budget
 		return false;
 	
 	if(p.getCategory().equals("Copri materasso"))// && controllore.containsKey("Copri materasso"))
-			if(controllore.get("Copri materasso")>2)
+			if(controllore.get("Copri materasso")>1)
 		return false;
 	
 	if(p.getCategory().equals("Cuscino culla"))// &&  controllore.containsKey("Cuscino culla"))
@@ -139,7 +141,7 @@ private boolean aggiuntaValida(Product p, List<Product> candidata2, float budget
 		return false;
 	
 	if(p.getCategory().equals("Cuscino culla"))// && controllore.containsKey("Cuscino culla"))
-			if(controllore.get("Cuscino culla")>1)
+			if(controllore.get("Cuscino culla")>0)
 		return false;
 	
 	if(p.getCategory().equals("Bavetta"))// && controllore.containsKey("Bavetta"))
@@ -147,7 +149,7 @@ private boolean aggiuntaValida(Product p, List<Product> candidata2, float budget
 		return false;
 	
 	if(p.getCategory().equals("Cuffia"))// && controllore.containsKey("Cuffia"))
-			if(controllore.get("Cuffia")>1)
+			if(controllore.get("Cuffia")>0)
 		return false;
 	
 	if(p.getCategory().equals("Vestitino "))// && controllore.containsKey("Vestitino "))
@@ -187,8 +189,9 @@ private boolean isCompleta(List<Product> candidata2) {
 	if(controllore.get("Fascia ombellicale")<1 || controllore.get("Lenzuolino culla")<1|| controllore.get("Copertina culla")<1)
 		return false;
 	
-	if(!(controllore.get("Body")==controllore.get("Tutina") && controllore.get("Body")==controllore.get("Ghettina") && controllore.get("Body")==controllore.get("Calzini") && controllore.get("Calzini")>2))
+	if(!(controllore.get("Body")==controllore.get("Tutina") &&controllore.get("Body")>1)  || controllore.get("Ghettina")<2)
 		return false;
+
 		
 	return true;
 }
